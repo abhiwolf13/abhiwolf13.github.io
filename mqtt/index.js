26d5b392e6832a70d2a553429affdb6df608631e
Abhi Lad <42882493+abhiwolf13@users.noreply.github.com>
2021-04-22 17:46:46 +0530

    Add files via upload

--- mqtt/index.js
+++ mqtt/index.js
@@ -15,7 +15,6 @@ slider1.oninput = function() {
     msgtosend=new Paho.MQTT.Message(JSON.stringify(slider1.value));
     msgtosend.destinationName=publisher;
     mqtt.send(msgtosend);
-    console.log('a');
   }
 
 // Mqtt connection to broker
@@ -24,24 +23,24 @@ function MQTTconnect(sub,pub){
     mqtt=new Paho.MQTT.Client('broker.emqx.io',8084,pub);
     mqtt.connect({timeout:3,
         useSSL: true,
-        onSuccess:function(){mqtt.subscribe(sub);publisher=pub;mqttinfo.innerHTML='Subscribed to: '+sub+' Publishing to: '+pub;connected=true ;console.log('connected');},
+        onSuccess:function(){mqtt.subscribe(sub);     setInterval(
+            mqtt.onMessageArrived=function(msg){
+            try{
+                msg=msg.payloadString;
+                msg=JSON.parse(msg);
+                output.innerHTML=msg;
+            }
+            catch(err){
+
+            }
+            
+        }
+        ,500);;publisher=pub;mqttinfo.innerHTML='Subscribed to: '+sub+' Publishing to: '+pub;connected=true ;console.log('connected');},
         onFailure:function(){setTimeout(MQTTconnect,500)}});
 
 }
 
-if(connected){
 
-    setInterval(
-        mqtt.onMessageArrived=function(msg){
-        console.log('a');
-        msg=msg.payloadString;
-        msg=JSON.parse(msg);
-        output.innerHTML=msg;
-        console.log(typeof msg);
-        
-    }
-    ,500);
-}
 
 
 
